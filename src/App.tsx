adc4ca7735ce21e40eb629e6b8cf63ee2cc123cc
cuebish <humberto@mando-media.com>
2025-02-24 22:19:05 -0400

    20230224 Cognito Form Update

--- src/App.tsx
+++ src/App.tsx
@@ -275,7 +275,9 @@ function App() {
                   </div>
                 </div>
                 <a 
-                  href="/register" 
+                  href="https://www.cognitoforms.com/AMIGOSDELAPLACITAGOLFCLASSICINC/_2025PlacitaOpenInscriptionForm"
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className="inline-block bg-green-600 text-white px-6 py-2 rounded-lg hover:bg-green-700 transition-colors mt-4"
                 >
                   Register Now
